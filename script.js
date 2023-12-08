c6842626383841266a38e18b13848d2331cda4c0
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2023-12-09 00:47:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+function test(first,second){
+	//console.log("inside function");
+		var n1=(2*first)
+			n1+=(second*1);
+		return n1;
+};
+var first=prompt("enter first");
+var second=prompt("enter second");
+alert("The final result is: "+test(first,second));
